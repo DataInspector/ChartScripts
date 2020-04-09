4450d673702f9b8a04dfce79057d9a51a10af9d9
lauriegeraghtygreen <62985947+lauriegeraghtygreen@users.noreply.github.com>
2020-04-09 08:07:52 +0100

    Update ScrapeTidy.R

--- ScrapeTidy.R
+++ ScrapeTidy.R
@@ -12,10 +12,10 @@ uschart$title <- as.character(uschart$title)
 ukchart$title <- as.character(ukchart$title)
 uschart$artist <- as.character(uschart$artist)
 ukchart$artist <- as.character(ukchart$artist)
-uschart$artist <- str_to_upper(uschart$artist)
-uschart$title <- str_to_upper(uschart$title)
+uschart$artist <- str_to_upper(uschart$artist) # UK charts are in upper case so changing US to match
+uschart$title <- str_to_upper(uschart$title) # UK charts are in upper case so changing US to match
 
-# filter to start from a common point
+# filter to start from a common point when the UK charts started. Pulling 50 places not the full US 100 as at the beginning the UK only had 50
 uschart <- filter(uschart, date >= '1960-03-11', rank <= 50)
 ukchart <- filter(ukchart, date >= '1960-03-11', rank <= 50)
 
@@ -32,7 +32,7 @@ ukchart$artist[ukchart$artist == 'THE FOUR TOPS'] <- 'FOUR TOPS'
 ukchart$artist[ukchart$artist == 'MARY J BLIGE'] <- 'MARY J. BLIGE'
 uschart$artist[uschart$artist == 'DARYL HALL JOHN OATES'] <- 'DARYL HALL AND JOHN OATES'
 
-# simplify artist names to remove featuring and With
+# simplify artist names to remove 'Featuring' 'Feat' and 'With The'
 uschart$artist.temp <- as.numeric(regexpr(' FEAT', uschart$artist))
 uschart$artist.temp[uschart$artist.temp==-1] <- as.numeric(regexpr(' WITH THE', uschart$artist[uschart$artist.temp==-1]))
 uschart$artist.temp[uschart$artist.temp<0] <- 100 # set the string length to 100 where 'FEAT' or 'WITH THE' isn't found so it isn't trimmed
